b799bc874ab873897b7c808ec8c2dd0fb35d1eab
lukewilson <lukewilson@utexas.edu>
2022-05-12 01:53:08 -0500

    new testz

--- test/test_flask.py
+++ test/test_flask.py
@@ -9,8 +9,15 @@ def test_flask_running():
 	responce = requests.get('http://' + FLASK_ADDRESS + '/')
 	assert responce.status_code == 200
 
-def test_redis_database():
+def test_redis_database_running():
 	rd = redis.StrictRedis(host=REDIS_IP,port=REDIS_PORT)
 	assert len(rd.keys()) > 0
 
-#def test_worker
+def test_create_operation():
+	rd = redis.StrictRedis(host=REDIS_IP,port=REDIS_PORT)
+	requests.post('http://' + FLASK_ADDRESS + '/create/sol00069 ')
+	responce = requests.get('http://' + FLASK_ADDRESS + '/read/sol00069 ')
+	assert responce.content != b'key does not exist.\n'
+
+
+
